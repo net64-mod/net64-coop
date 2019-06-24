ad673678e9aa4389af0471902266083e6010ec4b
Henrik <henrik-public@gmx.net>
2019-06-24 17:09:36 +0200

    Add missing namespace qualifiers

--- src/types.hpp
+++ src/types.hpp
@@ -12,16 +12,16 @@
 
 
 // Unsigned integer types
-using u8  = uint8_t;
-using u16 = uint16_t;
-using u32 = uint32_t;
-using u64 = uint64_t;
+using u8  = std::uint8_t;
+using u16 = std::uint16_t;
+using u32 = std::uint32_t;
+using u64 = std::uint64_t;
 
 // Signed integer types
-using i8  = int8_t;
-using i16 = int16_t;
-using i32 = int32_t;
-using i64 = int64_t;
+using i8  = std::int8_t;
+using i16 = std::int16_t;
+using i32 = std::int32_t;
+using i64 = std::int64_t;
 
 // Floating point types
 using f32 = float;
