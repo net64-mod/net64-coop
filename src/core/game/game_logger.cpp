cdc9484d6476202d9352b59317506872c3766700
Henrik <henrik-public@gmx.net>
2019-07-23 19:41:10 +0200

    Add GameLogger
    Forwards SM64 log messages

---
+++ src/core/game/game_logger.cpp
@@ -0,0 +1,48 @@
+//
+// Created by henrik on 12.07.19
+// Copyright 2019 Net64 Coop Project
+// Licensed under GPLv3
+// Refer to the LICENSE file included
+//
+
+#include "game_logger.hpp"
+
+#include <cstring>
+
+
+namespace Core::Game
+{
+
+static std::size_t strnlen(const char* str, std::size_t max_len)
+{
+    std::size_t len{};
+    for(; len < max_len && str[len] != '\0'; ++len){}
+    return len;
+}
+
+
+GameLogger::GameLogger():
+MessageListener{
+    GameMessage::LOG_APPEND,
+    GameMessage::LOG_END
+}{}
+
+void GameLogger::handle_message(const n64_message_t& message)
+{
+    switch(message.msg_type)
+    {
+    case GameMessage::LOG_APPEND:
+        text_buf_ += std::string(reinterpret_cast<const char*>(message.msg_data),
+                                 strnlen(reinterpret_cast<const char*>(message.msg_data), MSG_DATA_LEN));
+        break;
+    case GameMessage::LOG_END:
+        text_buf_ += std::string(reinterpret_cast<const char*>(message.msg_data),
+                                 strnlen(reinterpret_cast<const char*>(message.msg_data), MSG_DATA_LEN));
+        logger()->info(text_buf_);
+        text_buf_.clear();
+        break;
+    default: break;
+    }
+}
+
+}
