0635d68cca92dd068326f2a782a711f6f1886a78
Henrik <henrik-public@gmx.net>
2019-09-03 20:16:16 +0200

    Add Net64's fixed addresses and magic number

--- src/core/game/net64_header.hpp
+++ src/core/game/net64_header.hpp
@@ -21,11 +21,23 @@ struct net64_header_t
 {
     u16 compat_version;
     u16 version;
-    Memory::N64Ptr<MsgQueue::SharedState> receive_queue,
-                                          send_queue;
+    Memory::NestedPtr<MsgQueue::SharedState> receive_queue,
+                                             send_queue;
 };
 
 /// Compatibility version of the client
 constexpr u16 CLIENT_COMPAT_VER{0};
 
+/// Magic number indicating net64 has been successfully initialized
+constexpr u32 MAGIC_NUMBER{0x43303050}; // "C00P"
+
+/// Fixed addresses in the game
+namespace FixedAddr
+{
+
+constexpr n64_addr_t HEADER_PTR{0x807FFFFC};      ///< Location of a pointer to net64's global state
+constexpr n64_addr_t MAGIC_NUMBER{0x807FFFF8};    ///< Location of net64's magic number
+
 }
+
+} // Core::Game
